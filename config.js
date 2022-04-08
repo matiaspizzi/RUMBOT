4f24c39244210cc6241c0a92246c3b739a91f1a2
ron2452 <matias.nahuel.pizzi@hotmail.com>
2022-04-08 05:33:19 -0300

    private key error 3

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ const config = {
         private_key_id: process.env.PRIVATE_KEY_ID,
         private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
         client_id: process.env.CLIENT_ID,
+        clientEmail: process.env.CLIENT_EMAIL,
         auth_uri: process.env.AUTH_URI,
         token_uri: process.env.TOKEN_URI,
         auth_provider_x509_cert_url: process.env.AUT_PROVIDER_X509_CERT_URL,
