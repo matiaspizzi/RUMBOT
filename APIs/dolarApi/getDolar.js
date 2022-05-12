d22ee42bed8740e6da8a66fe8be81f5f1260d596
ron2452 <matias.nahuel.pizzi@hotmail.com>
2022-05-11 21:01:32 -0300

    Arreglo api dolar

--- APIs/dolarApi/getDolar.js
+++ APIs/dolarApi/getDolar.js
@@ -6,10 +6,21 @@ const getDolar = async () => {
     let dolarData = []
     for(const elem of get){
         const res = await fetch(`https://api-dolar-argentina.herokuapp.com/api/`+elem)
-        const data = await res.json()
-        if(data.compra && data.compra != "No cotiza") data.compra = "$" + data.compra
-        data.venta = "$" + data.venta
-        dolarData.push(data)
+        if(res.status === 404){
+            const data = {
+                fecha: "Sin datos",
+                compra: "Sin datos",
+                venta: "Sin datos",
+            }
+            dolarData.push(data)
+        } else {
+            const data = await res.json()
+            if(data.compra && data.compra != "No cotiza") data.compra = "$" + data.compra
+            if(data.venta){
+                data.venta = "$" + data.venta
+            }
+            dolarData.push(data)
+        }
     }
     return dolarData
 }
